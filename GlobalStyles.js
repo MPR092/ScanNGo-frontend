c4e9b49d1fc3c2217b70405a87271fe67aabf76c
MPR092 <mpr092@uregina.ca>
2023-07-23 11:06:57 -0600

    added global styles

---
+++ GlobalStyles.js
@@ -0,0 +1,44 @@
+/* fonts */
+export const FontFamily = {
+  latoBlack: "Lato_black",
+  latoRegular: "Lato_regular",
+  interRegular: "Inter_regular",
+  latoBold: "Lato_bold",
+  interBold: "Inter_bold",
+};
+/* font sizes */
+export const FontSize = {
+  size_5xl: 24,
+  size_2xs: 11,
+  size_sm: 14,
+  size_xs: 12,
+  size_mini: 15,
+  size_lg: 18,
+  size_xl: 20,
+  size_5xs: 8,
+  size_4xs: 9,
+  size_25xl: 44,
+};
+/* Colors */
+export const Color = {
+  white: "#fff",
+  gray: "#181725",
+  dark: "#000",
+  darkgray: "#a1a1a1",
+  blueviolet: "#6342e8",
+  ghostwhite: "#f1f4fb",
+};
+/* Paddings */
+export const Padding = {
+  p_mini: 15,
+};
+/* border radiuses */
+export const Border = {
+  br_21xl: 40,
+  br_37xl: 56,
+  br_5xs: 8,
+  br_11xl: 30,
+  br_8xs: 5,
+  br_9xs: 4,
+  br_6xs: 7,
+};

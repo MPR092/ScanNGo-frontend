6c5dcebd800c935b679a0d92ba3a634847f74655
MPR092 <mpr092@uregina.ca>
2023-07-23 11:08:33 -0600

    added email validation

---
+++ Shared/emailValid.js
@@ -0,0 +1,7 @@
+function isValidEmail(email) {
+    // Regular expression to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+export default isValidEmail;

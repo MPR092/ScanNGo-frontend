b4ccecb386cb7a6aae1b14797152c0dc37135ab5
MPR092 <mpr092@uregina.ca>
2023-07-27 12:19:14 -0600

    url for android updated

--- assets/common/BaseUrl.js
+++ assets/common/BaseUrl.js
@@ -3,7 +3,7 @@ import { Platform } from "react-native";
 let baseURL = '';
 
 {Platform.OS == 'android'
-? baseURL = 'http://10.0.2.2:3000/api/v1'
+? baseURL = 'http://192.168.0.18:3000/api/v1'
 : baseURL = `http://192.168.0.18:3000/api/v1`
 }
 

0d688ec5c12d2e28e00de12196b9f3f9aeb120b1
MPR092 <mpr092@uregina.ca>
2023-07-31 12:48:01 -0600

    typo error in import fixed

--- Redux/store.js
+++ Redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducer, applyMiddleware, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 
